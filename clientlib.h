e96833e55534a7d3e1a0b94873da3d5e08b4d3c8
Sara Weill <saracweill@gmail.com>
2022-04-04 09:32:59 -0400

    client refactoring

--- clientlib.h
+++ clientlib.h
@@ -7,16 +7,16 @@
 
 int create_handshake_xx_1(struct magpie_packet* packet, struct magpie_context* context);
     
-int handle_handshake_xx_2(struct magpie_packet* packet, struct magpie_context* context);
-
-int create_handshake_xx_3(struct magpie_packet* packet, struct magpie_context* context);
+int handle_handshake_xx_2(struct magpie_packet* out_packet, struct magpie_packet* in_packet, struct magpie_context* context);
 
 int create_read_req(struct magpie_packet* packet, struct magpie_context* context);
 
-int handle_read_response(struct magpie_packet* packet, struct magpie_context* context);
+int handle_read_response(struct magpie_packet* out_packet, struct magpie_packet* in_packet, struct magpie_context* context);
 
 int create_write_req(struct magpie_packet* packet, struct magpie_context* context);
 
-int create_write_data(struct magpie_packet* packet, struct magpie_context* context);
+int create_write_data(struct magpie_packet* out_packet, struct magpie_packet* in_packet, struct magpie_context* context);
+
+int magpie_client_handle_packet(struct magpie_packet* out_packet, struct magpie_packet* in_packet, struct magpie_context* context);
 
 #endif
